45f9c9fd54602ca2b249c96521bfe16b2a17721a
AJ-Brown-InTech <ajalantbrown@gmail.com>
2023-01-31 09:22:40 -0600

    camera page layout

--- src/Screens/Home/Post/Post.js
+++ src/Screens/Home/Post/Post.js
@@ -1,26 +1,13 @@
 import {Component} from 'react'
 import {Text, View} from 'react-native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import styles from '../../../styles/styles'
-const Tab = createBottomTabNavigator();
-
-function MyTabs() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
-    </Tab.Navigator>
-  );
-}
-
-
-
+import Camera from './Camera'
 class Feed extends Component {
 render(){
     return(
         <View style={styles.screen}>
             <Text >Post Page</Text>
-  
+              <Camera/>
            </View>
            
            )
